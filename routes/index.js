c07b967909250ac4e742c4f97f0aa1d4f26be2ed
Henrik Hellerøy <henrik.helleroy@bekk.no>
2016-02-26 17:56:18 +0100

    Define basic endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.send('This is not the endpoint you are looking for.');
+});
+
+router.post('/drink', function (req, res) {
+    res.send('Get yo drink on brah');
+});
+
+router.post('/refill', function (req, res) {
+    res.send('REUP!');
 });
 
 module.exports = router;
