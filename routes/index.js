aa58cb2e19db3948287567dfd0ee5cdff6a058f7
Henrik Hellerøy <henrik.helleroy@bekk.no>
2016-02-26 20:07:43 +0100

    Drink drinks with the /drink endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var STARTING_DRINKS = 3;
+
+var db = {};
+
 router.get('/', function (req, res, next) {
     res.send('This is not the endpoint you are looking for.');
 });
 
 router.post('/drink', function (req, res) {
-    res.send('Get yo drink on brah');
+    var id = getUUID(req);
+
+    if (isRegistered(id)) {
+        var drinksLeft = drink(id);
+
+        if (drinksLeft < 0) {
+            res.send('You are out of drinks, friendo.');
+            return;
+        }
+
+        if (drinksLeft === 0) {
+            res.send('This is your last drink. (You can refill *wink wink*)');
+            return;
+        }
+
+        res.send('You have ' + db[id] + ' drinks left. Enjoy!');
+    } else {
+        db[id] = STARTING_DRINKS;
+        drink(id);
+        res.send('You have ' + db[id] + ' drinks left. Enjoy!');
+    }
 });
 
 router.post('/refill', function (req, res) {
     res.send('REUP!');
 });
+
+function getUUID(req) {
+    return req.body.uuid;
+}
+
+function fillUp(id) {
+    db[id] = STARTING_DRINKS;
+}
+
+function isRegistered(id) {
+    return db[id] !== undefined;
+}
+
+function drink(id) {
+    db[id]--;
+    return db[id];
+}
 
 module.exports = router;
